71c5f8a92a1b544463fdb38617f602e2476d72fe
edidiesky <essienedidiong1000@gmail.com>
2024-06-13 09:22:50 -0700

    new fixes

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,21 @@ import express from "express";
 import path from "path";
 
 import cors from "cors";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-import { createServer } from 'node:http'
+import { createServer } from "node:http";
 import { Server } from "socket.io";
-
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000"
-  }
+    origin: process.env.WEB_ORIGIN,
+  },
 });
-
 
 import { errorHandler, NotFound } from "./middleware/error-handler.js";
 
@@ -33,21 +31,19 @@ app.use(
 // middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-import Auth from './routes/authRoute.js'
-import userAuth from './routes/userRoute.js'
-import roomRoute from './routes/roomRoutes.js'
-import reservationRoute from './routes/reservationsRoute.js'
+import Auth from "./routes/authRoute.js";
+import userAuth from "./routes/userRoute.js";
+import roomRoute from "./routes/roomRoutes.js";
+import reservationRoute from "./routes/reservationsRoute.js";
 import uploadRoute from "./routes/uploadRoute.js";
 
-
-app.use('/api/v1/auth', Auth)
-app.use('/api/v1/user', userAuth)
-app.use('/api/v1/room', roomRoute)
+app.use("/api/v1/auth", Auth);
+app.use("/api/v1/user", userAuth);
+app.use("/api/v1/room", roomRoute);
 app.use("/api/v1/message", reservationRoute);
 app.use("/api/v1/upload", uploadRoute);
-
 
 // // Middlewares
 app.use(NotFound);
@@ -55,67 +51,60 @@ app.use(errorHandler);
 
 // function that check sif the userId is included in the users array else it add the user id and scoket Id
 
-let users = []
+let users = [];
 const addUserId = (userId, socketId) => {
   // check if the object: {yserId, socketId} is being found in the usres array
   // if not found add it to the users array
- const userExits = users.find((user) => user.userId === userId);
+  const userExits = users.find((user) => user.userId === userId);
   if (!userExits) {
     users.push({ userId, socketId });
   }
-}
+};
 
 const RemoveUser = (socketId) => {
   // check if the object: {yserId, socketId} is being found in the usres array
   // if not found add it to the users array
-  users = users?.filter((user) => user?.socketId !== socketId)
-
-}
+  users = users?.filter((user) => user?.socketId !== socketId);
+};
 
 const getASpecificUser = (userId) => {
   // console.log(users)
 
   // return users?.filter((obj) => obj.userId === userId)
   return users.find((user) => user.userId === userId);
-}
+};
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
   // io.emit('message','Connected form the backend and testing sending of the data form the socket server')
-  socket.on('addUserId', (id) => {
-    addUserId(id, socket?.id)
-    io.emit('getAllConnectedUser', users)
-  })
+  socket.on("addUserId", (id) => {
+    addUserId(id, socket?.id);
+    io.emit("getAllConnectedUser", users);
+  });
   // socket.on('addUserId', (id) => console.log(id))
 
   // // get the userId connected from the client and send the users back to the client
 
-
   // // send message to a speco=ific user
-  socket.on('sendMessage', ({ receiverId, senderId, text }) => {
+  socket.on("sendMessage", ({ receiverId, senderId, text }) => {
     // get the specific usre u intend to send the message to
-    const user = getASpecificUser(receiverId)
+    const user = getASpecificUser(receiverId);
     // console.log(user, users)
     // console.log(user?.socketId)
     // console.log({ receiverId, senderId, text })
-    io.to(user?.socketId).emit('getMessage', {
+    io.to(user?.socketId).emit("getMessage", {
       receiverId: receiverId,
       text: text,
-    })
-
-  })
-
-  socket.on('disconnect', () => {
-    console.log('🔥: A user disconnected');
-    RemoveUser(socket?.id)
-    io.emit('getAllConnectedUser', users)
-
+    });
   });
 
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+    RemoveUser(socket?.id);
+    io.emit("getAllConnectedUser", users);
+  });
 });
-
 
 // addUserId(id, socket?.id)
 server.listen(4000, () => {
